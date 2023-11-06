4391c938c826c5eb2f8d8405734c3eb790595503
Eima Akhter <eimarobot721@gmail.com>
2023-11-06 11:32:39 +0600

    last update

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -18,6 +18,7 @@ function calculateTriangleArea() {
     // show triangle area
     const areaSpan = document.getElementById('triangle-area');
     areaSpan.innerText = area;
+    addToCalculationEntry('Triangle',area);
 }
 
 function calculateRectangleArea (){
@@ -33,6 +34,12 @@ const lengthValueText = lengthField.value;
 const length = parseFloat (lengthValueText);
 console.log(length);
 
+// validate input field length
+if(isNaN(length) || isNaN(width)){
+    alert('please insert a number ');
+    return;
+}
+
 // calculate rectangle area 
 const area = width *length;
 console.log(area);
@@ -40,6 +47,7 @@ console.log(area);
 // show rectangle area 
 const areaSpan = document.getElementById('rectangle-area');
 areaSpan.innerText = area;
+addToCalculationEntry('Rectangle',area);
 
 
 }
@@ -47,12 +55,19 @@ areaSpan.innerText = area;
 // reusable function -- reduce duplicate code 
 function calculateParallelogramArea (){
    const base =getInputValue('parallelogram-base');
-   console.log(base);
+//    console.log(base);
    const height = getInputValue('parallelogram-height');
-   console.log(height);
+//    console.log(height);
+
+// validate input : base and height 
+if (isNaN(base) || isNaN (height)){
+    alert('Please insert a number ');
+    return
+}
 
    const area = base * height;
    setElementInnerText('parallelogram-area',area);
+   addToCalculationEntry('parallelogram' ,area);
 }
 
 function calculateEllipseArea(){
@@ -61,7 +76,9 @@ function calculateEllipseArea(){
     const minorRadius = getInputValue('ellipse-minor-radius');
     console.log(minorRadius);
     const area = 3.14 * majorRadius * minorRadius;
-    setElementInnerText('ellipse-area',area);
+    const areaTwoDecimal = area.toFixed(2);
+    setElementInnerText('ellipse-area',areaTwoDecimal);
+    addToCalculationEntry('Ellipse',areaTwoDecimal);
 }
 
 // reusable get input field value in number 
@@ -77,3 +94,31 @@ function setElementInnerText(elementId,area){
     const element =document.getElementById(elementId);
     element.innerText = area;
 }
+
+// add to calculation entry
+
+/*
+*1.get the element where you want to add the dynamic html
+*2.create an element you want to add
+*3.if needed add some class
+*4.add inner HTML .it would be dynamic Template string 
+* 5.append the created element as a child of the parent 
+**/ 
+function addToCalculationEntry(areaType, area){
+    console.log(areaType + '  ' + area);
+    const calculationEntry = document.getElementById('calculation-entry');
+    const count = calculationEntry.childElementCount;
+
+    const p = document.createElement('p');
+    p.classList. add('my-4')
+    p.innerHTML = ` ${count + 1}. ${areaType}  ${area} cm <sup>2</sup> <button class= "btn btn-success btn-small">Convert</button>`;
+    calculationEntry.appendChild (p);
+
+}
+
+// Data validation 
+/* 
+1.set the proper type of the input field (number, text, email)
+*2. check type using typeof 
+*3.NaN means Not a Number .isNaN is checking whether a input is not a number or not 
+**/ 
